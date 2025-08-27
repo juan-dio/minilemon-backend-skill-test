aefa4b1e12779d871825e7bf7906c63a4dccdfc1
juan-dio <juanaxl432@gmail.com>
2025-08-27 21:59:00 +0700

    try add explorer to swagger

--- swagger.js
+++ swagger.js
@@ -22,7 +22,7 @@ const options = {
 const swaggerSpec = swaggerJsdoc(options);
 
 function swaggerDocs(app, port) {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, { explorer: true }));
   // app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
   app.get("/api-docs.json", (req, res) => {
     res.setHeader("Content-Type", "application/json");
